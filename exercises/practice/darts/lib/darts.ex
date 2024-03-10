ca92584a6cafb58eff854af1165acdf8be88c894
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-03-10 14:09:15 +0530

    feat: Update darts.ex
    Signed-off-by: harish-datla <36021808+harish-datla@users.noreply.github.com>

--- exercises/practice/darts/lib/darts.ex
+++ exercises/practice/darts/lib/darts.ex
@@ -6,5 +6,12 @@ defmodule Darts do
   """
   @spec score(position) :: integer
   def score({x, y}) do
+    distance = x*x + y*y
+    cond do
+      distance > 100 ->  0
+      distance <= 100 and distance > 25 -> 1
+      distance <= 25 and distance > 1 -> 5
+      distance <= 1 -> 10
+    end
   end
 end
